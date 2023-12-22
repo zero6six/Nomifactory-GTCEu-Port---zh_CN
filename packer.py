2903856d193598fdeba5ca09f67681fc5ff42ada
zero6six <zero6six@qq.com>
2023-12-23 00:01:52 +0800

    重构成打包时选择是否双语

--- packer.py
+++ packer.py
@@ -3,6 +3,54 @@ import zipfile
 
 # 本脚本大部分由 ChatGPT 提供。
 
+def merge_lang_files():
+    import re
+    pattern = r'nomifactory\.quest\.(expert|normal)\.db\.\d+\.desc='
+    
+    # 读取file1
+    with open('assets//questbook//lang//en_us.lang', 'r', encoding='utf-8') as f1:
+        lang1 = f1.readlines()
+
+    # 读取file2
+    with open('assets//questbook//lang//zh_cn.lang', 'r', encoding='utf-8') as f2:
+        lang2 = f2.readlines()
+
+    # 创建新的lang文件
+    new_lang = []
+
+    # 遍历file2的每一行
+    # lang1 为英文 lang2 为中文
+    for line2 in lang2:
+        # 检查是否为目标键
+        match = re.match(pattern, line2)
+        if match:
+            # 获取键名
+            key = match.group()[:-1]
+
+            # 遍历file1的每一行
+            for line1 in lang1:
+                # 检查是否为目标键
+                if line1.startswith(key):
+                    # 获取键值
+                    value1 = line1.split('=')[1].strip()
+
+                    # 在值之前添加%n%n%n%n
+                    new_value = '§r%n%n%n%n' + value1
+
+                    # 追加到file2的对应键的值之前
+                    line2 = line2.rstrip('\n') + new_value + '\n'
+
+                    # 结束内部循环
+                    break
+
+        # 将处理后的行添加到新lang文件中
+        new_lang.append(line2)
+
+    # 将新lang文件保存为新文件
+    new_file = 'output//tempQB.lang'
+    with open(new_file, 'w', encoding='utf-8') as f:
+        f.writelines(new_lang)
+
 def create_resource_pack():
     # 当前工作目录路径
     workspace_path = os.getcwd()
@@ -19,7 +67,12 @@ def create_resource_pack():
     for root, _, files in os.walk(assets_path):
         for file in files:
             file_path = os.path.join(root, file)
-            zip_file.write(file_path, os.path.relpath(file_path, workspace_path))
+            if file_path.endswith(r"questbook\lang\zh_cn.lang") and whetherBilingual:
+                merge_lang_files()
+                zip_file.write('output//tempQB.lang', os.path.relpath(file_path, workspace_path))
+                os.remove('output//tempQB.lang')
+            else:
+                zip_file.write(file_path, os.path.relpath(file_path, workspace_path))
 
     # 添加 pack.mcmeta 文件到压缩包
     pack_mcmeta_path = os.path.join(workspace_path, "pack.mcmeta")
@@ -34,5 +87,6 @@ def create_resource_pack():
 
     print("资源包已创建：", os.path.join(output_path, output_name))
 
+whetherBilingual = True
 # 运行创建资源包的函数
 create_resource_pack()
